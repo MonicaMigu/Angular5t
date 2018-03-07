871055c074e5680e8087ee1e6e223519839ed24f
MonicaMigu <monica2014939@yahoo.com.tw>
2018-03-07 20:59:39 +0800

    Work to Lession2

---
+++ src/app/lesson1/lesson1.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-lesson1',
+  templateUrl: './lesson1.component.html',
+  styleUrls: ['./lesson1.component.css']
+})
+export class Lesson1Component implements OnInit {
+ abc = "";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/lesson2/lesson2.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lesson2',
+  templateUrl: './lesson2.component.html',
+  styleUrls: ['./lesson2.component.css']
+})
+export class Lesson2Component implements OnInit {
+  show = true;
+  score = 60;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  xxxx(){
+    this.show = !this.show;
+  }
+
+}
